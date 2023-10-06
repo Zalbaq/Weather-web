9aa5e0eba9bfa98b36a76ab469c87ea7c9252828
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-06 22:22:52 +0700

    add icon.js

---
+++ javascript/icon.js
@@ -0,0 +1,7 @@
+export const iconWeather = {
+  rainy: "../icon/animated/rainy-7.svg",
+  cloudy: "../icon/animated/cloudy-day-1.svg",
+  shiny: "../icon/animated/day.svg",
+  thunder: "../icon/animated/thunder.svg",
+  snowy: "../icon/animated/snowy-6.svg",
+};
