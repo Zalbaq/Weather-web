49e0bf2beba20c3aee1c17219523cb2992fe5351
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-08 13:57:33 +0700

    add function to get weather data and display it

--- javascript/index.js
+++ javascript/index.js
@@ -6,6 +6,53 @@ const url = {
   forecast: "http://api.openweathermap.org/data/2.5/forecast?",
 };
 
+const fetchDataWeather = async (location) => {
+  const weatherIcon = document.querySelector("#weather-icon > img");
+  const temp = document.querySelector("#temp");
+  const locationH2 = document.querySelector("#location");
+  const humidity = document.querySelector("#humidity > label");
+  const windSpeed = document.querySelector("#wind-speed > label");
+  const sunrise = document.querySelector("#sunrise > label");
+  const sunset = document.querySelector("#sunset > label");
+  const res = {
+    current: await fetch(url.current + `q=${location}` + `&appid=${apiKey}`),
+    forecast: await fetch(url.forecast + `q=${location}` + `&appid=${apiKey}`),
+  };
+  const data = {
+    current: await res.current.json(),
+    forecast: await res.forecast.json(),
+  };
+
+  const idCurrent = data.current.weather[0].id;
+  const timestampSunrise = data.current.sys.sunrise;
+  const timestampSunset = data.current.sys.sunset;
+
+  const realTimeSunrise = new Date(timestampSunrise * 1000);
+  const realTimeSunset = new Date(timestampSunset * 1000);
+
+  if (idCurrent >= 801) {
+    weatherIcon.src = iconWeather.cloudy;
+  } else if (idCurrent === 800) {
+    weatherIcon.src = iconWeather.shiny;
+  } else if (idCurrent >= 700) {
+    weatherIcon.src = iconWeather.mist;
+  } else if (idCurrent >= 600) {
+    weatherIcon.src = iconWeather.snowy;
+  } else if (idCurrent >= 500) {
+    weatherIcon.src = iconWeather.rainy;
+  } else if (idCurrent >= 300) {
+    weatherIcon.src = iconWeather.drizzle;
+  } else if (idCurrent >= 200) {
+    weatherIcon.src = iconWeather.thunder;
+  }
+
+  temp.textContent = `${(data.current.main.temp - 273.15).toFixed(2)}°`;
+  locationH2.textContent = `${data.current.name}`
+  humidity.textContent = `${data.current.main.humidity}%`;
+  windSpeed.textContent = `${data.current.wind.speed}Km/h`;
+  sunrise.textContent = `${realTimeSunrise.getHours()} : ${realTimeSunrise.getMinutes()} : ${realTimeSunrise.getSeconds()}`;
+  sunset.textContent = `${realTimeSunset.getHours()} : ${realTimeSunrise.getMinutes()} : ${realTimeSunrise.getSeconds()}`;
+};
 
 async function main() {
   const data = await fetchDataWeather("bali");
