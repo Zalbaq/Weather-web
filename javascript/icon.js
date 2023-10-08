abd62a782ff3c82a5f22db82799a4584a9714b98
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-08 13:54:06 +0700

    add new path icon

--- javascript/icon.js
+++ javascript/icon.js
@@ -1,7 +1,9 @@
 export const iconWeather = {
-  rainy: "../icon/animated/rainy-7.svg",
+  drizzle: "../icon/animated/rainy-7.svg", 
+  rainy: "../icon/animated/rainy-6.svg",
   cloudy: "../icon/animated/cloudy-day-1.svg",
   shiny: "../icon/animated/day.svg",
   thunder: "../icon/animated/thunder.svg",
   snowy: "../icon/animated/snowy-6.svg",
+  mist: "../icon/animated/cloudy.svg",
 };
