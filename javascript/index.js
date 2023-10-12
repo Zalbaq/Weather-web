31c6f533b8ce9cebee4def4c8c96dc5b02bdd2ef
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-12 08:02:48 +0700

    change request protocol API

--- javascript/index.js
+++ javascript/index.js
@@ -2,8 +2,8 @@ import { iconWeather } from "./icon.js";
 
 const apiKey = "d0b9a892411cffcf8d604e5fb4f7a332";
 const url = {
-  current: "http://api.openweathermap.org/data/2.5/weather?",
-  forecast: "http://api.openweathermap.org/data/2.5/forecast?",
+  current: "https://api.openweathermap.org/data/2.5/weather?",
+  forecast: "https://api.openweathermap.org/data/2.5/forecast?",
 };
 
 const fetchDataWeather = async (location) => {
