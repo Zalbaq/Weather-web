3a8939decc773f6a2d037ddd4e4ad94abf8762fa
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-13 22:25:45 +0700

    [fix] bug path of icon

--- javascript/icon.js
+++ javascript/icon.js
@@ -1,9 +1,9 @@
 export const iconWeather = {
-  drizzle: "../icon/animated/rainy-7.svg", 
-  rainy: "../icon/animated/rainy-6.svg",
-  cloudy: "../icon/animated/cloudy-day-1.svg",
-  shiny: "../icon/animated/day.svg",
-  thunder: "../icon/animated/thunder.svg",
-  snowy: "../icon/animated/snowy-6.svg",
-  mist: "../icon/animated/cloudy.svg",
+  drizzle: "./icon/animated/rainy-7.svg",
+  rainy: "./icon/animated/rainy-6.svg",
+  cloudy: "./icon/animated/cloudy-day-1.svg",
+  shiny: "./icon/animated/day.svg",
+  thunder: "./icon/animated/thunder.svg",
+  snowy: "./icon/animated/snowy-6.svg",
+  mist: "./icon/animated/cloudy.svg",
 };
